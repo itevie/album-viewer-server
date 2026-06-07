8b9470dacc9eb37da62bc185d8d78d69998db0e6
Isabella <izzyplays07@gmail.com>
2026-06-07 23:41:40 +0100

    big

--- src/web/flutter_bootstrap.js
+++ src/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1026013449" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3103351863" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
